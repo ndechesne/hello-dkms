9920cc8024876e1e3501d5ac2bf432b0792e49d1
Nicolas Dechesne <n-dechesne@ti.com>
2011-09-22 12:45:25 +0200

    Simple hello world dkms example
    Signed-off-by: Nicolas Dechesne <n-dechesne@ti.com>

---
+++ hello.c
@@ -0,0 +1,18 @@
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+static int __init hello_init(void)
+{
+        pr_info("Hello world.\n");
+        return 0;
+}
+
+static void __exit hello_exit(void)
+{
+        pr_info("Goodbye world.\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+
